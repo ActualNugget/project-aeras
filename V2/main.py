a99d8b048244160f1eb4b6356dff49ed76d1e40f
ActualNugget <isvaran@tuta.io>
2023-05-05 21:52:00 +0800

    pin settings

--- V2/main.py
+++ V2/main.py
@@ -1,4 +1,20 @@
+import RPi.GPIO as GPIO
 import led_display
 import weight_sensor
 
-led_display(6)
+# GPIO Setup
+GPIO.setmode(GPIO.BCM)
+display_pins = [17,27,22,10,9,11,5]
+dout_pin = 13
+pd_sck_pin = 6
+lift_up_pin = 
+lift_down_pin = 
+lift_close_pin = 
+
+level = 1
+
+try:
+    bruh
+    
+finally:
+    GPIO.cleanup()
