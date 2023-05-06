7bbbf982fe9e871ff93128402b2b2f7fa0a5960d
ActualNugget <isvaran@tuta.io>
2023-05-06 11:20:52 +0800

    double distance

--- V2/ultrasonic_dist.py
+++ V2/ultrasonic_dist.py
@@ -6,14 +6,25 @@ import time
 GPIO.setmode(GPIO.BCM)
  
 #set GPIO Pins
-GPIO_TRIGGER = 37
-GPIO_ECHO = 7
+trigger_pin_1 = 26
+echo_pin_1 = 4
+trigger_pin_2 = 3
+echo_pin_2 = 2
+
  
 #set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-def distance():
+GPIO.setup(trigger_pin_1, GPIO.OUT)
+GPIO.setup(trigger_pin_2, GPIO.OUT)
+GPIO.setup(echo_pin_1, GPIO.IN)
+GPIO.setup(echo_pin_2, GPIO.IN)
+
+def distance(GPIO_TRIGGER, GPIO_ECHO):
+    # Reset Trigger
+    # print("sleepin")
+    # GPIO.output(GPIO_TRIGGER, 0)
+    # time.sleep(2)
+    # GPIO.output(GPIO_TRIGGER, 1)
+
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
  
@@ -23,31 +34,43 @@ def distance():
  
     StartTime = time.time()
     StopTime = time.time()
- 
+
     # save StartTime
     while GPIO.input(GPIO_ECHO) == 0:
         StartTime = time.time()
- 
+
     # save time of arrival
     while GPIO.input(GPIO_ECHO) == 1:
         StopTime = time.time()
  
     # time difference between start and arrival
     TimeElapsed = StopTime - StartTime
+
     # multiply with the sonic speed (34300 cm/s)
     # and divide by 2, because there and back
     distance = (TimeElapsed * 34300) / 2
  
     return distance
  
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
+try:
+    count = 0
+    while True:
+        exit = distance(trigger_pin_1, echo_pin_1)
+        entry = distance(trigger_pin_2, echo_pin_2)
+        print ("Entry = %.1f cm" % entry)
+        print ("Exit = %.1f cm" % exit)
+
+        if entry < 7:
+            count += 1
             time.sleep(1)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+        elif exit < 7:
+            count -= 1
+            time.sleep(1)
+        print(count)
+
+        time.sleep(0.1)
+
+    # Reset by pressing CTRL + C
+except KeyboardInterrupt:
+    print("Measurement stopped by User")
+    GPIO.cleanup()
