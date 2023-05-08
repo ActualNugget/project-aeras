91e1c0ef65ac9d87bbc4af97bfbecb5c19983ed4
ActualNugget <isvaran@tuta.io>
2023-05-08 17:39:47 +0800

    Restored website to unthreaded

--- website/__init__.py
+++ website/__init__.py
@@ -1,31 +1,8 @@
 from flask import Flask
 
-class Publisher:
-    def __init__(self):
-        self.subscribers = set()
 
-    def register(self, subscriber):
-        self.subscribers.add(subscriber)
-    
-    def notify(self, message):
-        for subscriber in self.subscribers:
-            subscriber.update(message)
-
-class Subscriber:
-    def __init__(self, app, variable_name) -> None:
-        self.app = app
-        self.variable_name = variable_name
-    
-    def update(self, message):
-        setattr(self.app, self.variable_name, message)
-
-
-def create_app(publisher):
+def create_app():
     app = Flask(__name__)
-    # publisher = Publisher()
-    subscriber = Subscriber(app, 'counters')
-    publisher.register(subscriber)
-
     app.config['SECRET_KEY'] = 'hhjsjiwueuioqwuro'
 
     from .views import views
@@ -35,4 +12,3 @@ def create_app(publisher):
     app.register_blueprint(auth, url_prefix='/')
 
     return app
-
--- website/views.py
+++ website/views.py
@@ -1,23 +1,16 @@
 from flask import Blueprint, render_template
 
-# from V2.counter import level_pax
-# from V2.counter import lift_pax
+from V2.main import level_pax
+from V2.main import lift_pax
 
 # Current floor data:
 
 
-lift_pax = 9
-level_pax = {1: 2,
-             2: 2,
-             3: 4,
-             4: 4,
-             5: 6}
-counters = {"lift": lift_pax, "levels": level_pax}
+lift_pax = 4
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():
-    global counters
-    return render_template("home.html", lift_pax=counters["lift"], level_pax=counters["levels"])
+    return render_template("home.html", lift_pax=lift_pax, level_pax=level_pax)
