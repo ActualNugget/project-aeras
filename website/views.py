5da23321cc76682e32990be7e6837379ca5b87b6
xujialu <101682270+3nityR@users.noreply.github.com>
2023-05-08 10:35:26 +0800

    Changed formatting in views.py

--- website/views.py
+++ website/views.py
@@ -1,19 +1,21 @@
 from flask import Blueprint, render_template
+
 # from V2.main import level_pax
 # from V2.main import lift_pax
 
 # Current floor data:
 level_pax = {
-    1:3,
-    2:4,
-    3:5,
-    4:1,
-    5:4
-    }
+    1: 3,
+    2: 4,
+    3: 5,
+    4: 1,
+    5: 4
+}
 
 lift_pax = 4
 
 views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def home():
