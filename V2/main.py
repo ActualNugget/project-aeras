73e77c4762e207bbeacd972b34174203ee4aae08
ActualNugget <isvaran@tuta.io>
2023-05-09 12:54:33 +0800

    swapped to threading

--- V2/main.py
+++ V2/main.py
@@ -1,7 +1,7 @@
 from website import create_app
 from counter import counter
 from collections import defaultdict
-from multiprocessing import Process
+from threading import Thread
 
 
 def flask_app(app):
@@ -17,8 +17,7 @@ counters = {"lift": lift_pax, "levels": level_pax}
 
 if __name__ == '__main__':
 
-    flask_proc = Process(target=flask_app, args=(app, ))
-    flask_proc.start()
-
-    count_proc = Process(target=counter)
+    count_proc = Thread(target=counter)
     count_proc.start()
+
+    flask_app(app)
